f5663d6891780a34914178094065b1221bf45c1c
meemee10 <meeraforlanguage@gmail.com>
2018-09-04 10:50:18 +0200

    modif html et js

--- ListMemo.js
+++ ListMemo.js
@@ -14,13 +14,67 @@
 	}
 
 
+
+
 //pour supprimer un élément de la liste
 	//on récupère les éléments qui sont associés à un bouton close
-	var close = document.getElementsByClassName("close");
+	/*var close = document.getElementsByClassName("close");
 	//on parcourt la liste
 	for (var i = 0; i < close.length; i++) {
 	  close[i].onclick = function() {
 		var div = this.parentElement;
 		div.style.display = "none";
 	  }
+	}*/
+	
+// Bouton add new task
+	
+	function insertRow(){
+		//on crée un élément de la liste
+		var li= document.createElement("li");
+		//on récupère ce que tape l'utilisateur
+		var input=document.getElementById("text").value;
+		//on rajoute cet "input" en tant qu'élément de la liste
+		var t= document.createTextNode(input);
+		li.appendChild(t);
+		var button=document.createElement("button");
+		var text=document.createTextNode("\u00D7");
+		button.className="close";
+		button.appendChild(text);
+		li.appendChild(button);
+		
+		
+		//si l'utilisateur ne rentre aucune donnée
+		if (input=== '') {
+			alert ("veuiller écrire quelquechose svp");
+		}
+		//sinon on rajoute l'élément à liste
+		else{
+			document.getElementById("menu").appendChild(li);
+		}
+		//document.getElementById("input").value="";
+		
+		
+		
+		/*var close = document.getElementsByClassName("close");
+		quand on clique sur le bouton close, l'élément est retiré
+		for(var i=0; i<close.length;i++) {
+			close[i].onclick=function() {
+				var div= this.parentElement;
+				div.style.display="none";
+			}
+		}*/
+		
+		
+		
+		
 	}
+	
+	document.getelementbyid("text").addEventListener("click", modifyText);
+	
+	function modifyText() {
+  alert("tedt");
+}
+	
+		
+			
